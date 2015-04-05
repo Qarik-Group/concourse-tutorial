ef8db9ef58602fee007cda0ab769442bc2b3434e
Dr Nic Williams <drnicwilliams@gmail.com>
2015-04-04 23:11:07 -0700

    WIP - 10 - deploy an app to CF

--- test/build.sh
+++ test/build.sh
@@ -6,11 +6,11 @@ set -e
 export ATC_URL=${ATC_URL:-"http://192.168.100.4:8080"}
 echo "Concourse API $ATC_URL"
 
-pushd 01_*
-fly execute -c task_hello_world.yml
-popd
-fly configure
-
+# pushd 01_*
+# fly execute -c task_hello_world.yml
+# popd
+# fly configure
+#
 #
 # pushd 02_*
 # yes y | fly configure -c pipeline.yml
@@ -34,10 +34,17 @@ fly configure
 # curl $ATC_URL/jobs/job-hello-world/builds -X POST
 # fly watch -j job-hello-world
 # popd
+#
+# pushd 08_*
+# yes y | fly configure -c pipeline.yml
+# curl $ATC_URL/jobs/job-fetch-resource/builds -X POST
+# fly watch -j job-fetch-resource
+# fly watch -j job-run-task
+# popd
 
-pushd 08_*
+pushd 10_*
 yes y | fly configure -c pipeline.yml
-curl $ATC_URL/jobs/job-hello-world/builds -X POST
-fly watch -j job-fetch-resource
-fly watch -j job-run-task
+curl $ATC_URL/jobs/job-fetch-app/builds -X POST
+fly watch -j job-fetch-app
+# fly watch -j job-run-task
 popd
