f13f1c79c52b1209a80c81abec764c509bf5df21
Dr Nic Williams <drnicwilliams@gmail.com>
2015-04-04 19:49:54 -0700

    fix test

--- test/build.sh
+++ test/build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 set -e
-
+# set -x
 
 export ATC_URL=${ATC_URL:-"http://192.168.100.4:8080"}
 echo "Concourse API $ATC_URL"
 
-# pushd 01_*
-# fly execute -c task_hello_world.yml
-# popd
+pushd 01_*
+fly execute -c task_hello_world.yml
+popd
+fly configure
+
 #
 # pushd 02_*
 # yes y | fly configure -c pipeline.yml
@@ -36,5 +38,6 @@ echo "Concourse API $ATC_URL"
 pushd 08_*
 yes y | fly configure -c pipeline.yml
 curl $ATC_URL/jobs/job-hello-world/builds -X POST
-fly watch -j job-hello-world
+fly watch -j job-fetch-resource
+fly watch -j job-run-task
 popd
