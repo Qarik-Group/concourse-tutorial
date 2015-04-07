861493d16bf5f50db25206f5baa043c255f61be9
Dr Nic Williams <drnicwilliams@gmail.com>
2015-04-07 15:31:02 -0700

    test/build.sh calls out to run.sh scripts

--- test/build.sh
+++ test/build.sh
@@ -6,58 +6,14 @@ set -e
 export ATC_URL=${ATC_URL:-"http://192.168.100.4:8080"}
 echo "Concourse API $ATC_URL"
 
-# pushd 01_*
-# fly execute -c task_hello_world.yml
-# popd
-# fly configure
-#
-#
-# pushd 02_*
-# yes y | fly configure -c pipeline.yml
-# curl $ATC_URL/jobs/job-hello-world/builds -X POST
-# fly watch -j job-hello-world
-# popd
-#
-# pushd 03_*
-# yes y | fly configure -c pipeline.yml
-# curl $ATC_URL/jobs/job-hello-world/builds -X POST
-# fly watch -j job-hello-world
-# popd
-#
-# pushd 06_*
-# yes y | fly configure -c pipeline.yml
-# # verify pipeline config
-# popd
-#
-# pushd 07_*
-# yes y | fly configure -c pipeline.yml
-# curl $ATC_URL/jobs/job-hello-world/builds -X POST
-# fly watch -j job-hello-world
-# popd
-#
-# pushd 08_*
-# yes y | fly configure -c pipeline.yml
-# curl $ATC_URL/jobs/job-fetch-resource/builds -X POST
-# fly watch -j job-fetch-resource
-# fly watch -j job-run-task
-# popd
-#
-# pushd 10_*
-# yes y | fly configure -c pipeline.yml
-# curl $ATC_URL/jobs/job-deploy-app/builds -X POST
-# fly watch -j job-deploy-app
-# popd
-#
-pushd 11_*
-spiff merge templates/pipeline-final.yml templates/pipeline-base.yml ../stub.yml > pipeline.yml
-yes y | fly configure -c pipeline.yml
-curl $ATC_URL/jobs/job-deploy-app/builds -X POST
-fly watch -j job-deploy-app
-popd
-
-# pushd 12_*
-# spiff merge templates/pipeline-final.yml templates/pipeline-base.yml ../stub.yml > pipeline.yml
-# yes y | fly configure -c pipeline.yml
-# curl $ATC_URL/jobs/job-test-deploy-app/builds -X POST
-# fly watch -j job-test-deploy-app
-# popd
+./01_*/run.sh
+./02_*/run.sh
+./03_*/run.sh
+./06_*/run.sh
+./07_*/run.sh simple
+./07_*/run.sh renamed
+./08_*/run.sh
+./10_*/run.sh
+./11_*/run.sh stub.yml
+./12_*/run.sh stub.yml
+./20_*/run.sh stub.yml
