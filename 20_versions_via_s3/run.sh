d3175d9a7449d408cf81859d3799c68f2b99eaa8
Dr Nic Williams <drnicwilliams@gmail.com>
2015-04-07 15:37:18 -0700

    20 - stage to display the semver resource

--- 20_versions_via_s3/run.sh
+++ 20_versions_via_s3/run.sh
@@ -9,19 +9,27 @@ realpath() {
     [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
 }
 
+usage() {
+  echo "USAGE: run.sh path/to/stub.yml [get-version|display-version]"
+  exit 1
+}
+
 stub=$1; shift
 if [[ "${stub}X" == "X" ]]; then
-  echo "USAGE: run.sh path/to/stub.yml"
-  exit 1
+  usage
 fi
 stub=$(realpath $stub)
 if [[ ! -f ${stub} ]]; then
-  echo "USAGE: run.sh path/to/stub.yml"
-  exit 1
+  usage
+fi
+
+stage=$1; shift
+if [[ "${stage}" != "get-version" && "${stage}" != "display-version" ]]; then
+  usage
 fi
 
 pushd $DIR
-  spiff merge templates/pipeline-final.yml templates/pipeline-base.yml ${stub} > pipeline.yml
+  spiff merge templates/pipeline-final.yml templates/pipeline-base-${stage}.yml ${stub} > pipeline.yml
   yes y | fly configure -c pipeline.yml
   curl $ATC_URL/jobs/job-bump-version/builds -X POST
   fly watch -j job-bump-version
