23bf20ddeed672ba08bb1bd634e21d67e9a26211
Dr Nic Williams <drnicwilliams@gmail.com>
2015-04-10 22:41:12 -0700

    46 - WIP spiff merge on upstream templates

---
+++ 46_bosh_manifest_spiff_merge/README.md
@@ -0,0 +1,4 @@
+45 - BOSH deploy
+================
+
+This section will show how to deploy a simple manifest to an existing BOSH.
---
+++ 46_bosh_manifest_spiff_merge/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+export ATC_URL=${ATC_URL:-"http://192.168.100.4:8080"}
+echo "Tutorial $(basename $DIR)"
+echo "Concourse API $ATC_URL"
+
+realpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+stub=$1; shift
+if [[ "${stub}X" == "X" ]]; then
+  echo "USAGE: run.sh path/to/stub.yml"
+  exit 1
+fi
+stub=$(realpath $stub)
+if [[ ! -f ${stub} ]]; then
+  echo "USAGE: run.sh path/to/stub.yml"
+  exit 1
+fi
+
+pushd $DIR
+  spiff merge templates/pipeline-final.yml templates/pipeline-base.yml ${stub} > pipeline.yml
+  yes y | fly configure -c pipeline.yml
+  curl $ATC_URL/jobs/job-spiff-merge/builds -X POST
+  fly watch -j job-spiff-merge
+popd
