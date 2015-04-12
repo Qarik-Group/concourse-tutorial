bc2a8f357709db44944ad38fb5818ed7f14cfed8
Dr Nic Williams <drnicwilliams@gmail.com>
2015-04-11 20:19:25 -0700

    46 - pipeline shows spiff merge output

--- 46_bosh_manifest_spiff_merge/run.sh
+++ 46_bosh_manifest_spiff_merge/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+stub=$1; shift
+stage=$1; shift
+
 set -e
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
@@ -11,19 +14,25 @@ realpath() {
     [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
 }
 
-stub=$1; shift
-if [[ "${stub}X" == "X" ]]; then
-  echo "USAGE: run.sh path/to/stub.yml"
+usage() {
+  echo "USAGE: run.sh path/to/stub.yml [show|save]"
   exit 1
+}
+
+if [[ "${stub}X" == "X" ]]; then
+  usage
 fi
 stub=$(realpath $stub)
 if [[ ! -f ${stub} ]]; then
-  echo "USAGE: run.sh path/to/stub.yml"
-  exit 1
+  usage
+fi
+
+if [[ "${stage}" != "show" && "${stage}" != "save" ]]; then
+  usage
 fi
 
 pushd $DIR
-  spiff merge templates/pipeline-final.yml templates/pipeline-base.yml ${stub} > pipeline.yml
+  spiff merge templates/pipeline-final.yml templates/pipeline-base-${stage}.yml ${stub} > pipeline.yml
   yes y | fly configure -c pipeline.yml
   curl $ATC_URL/jobs/job-spiff-merge/builds -X POST
   fly watch -j job-spiff-merge
