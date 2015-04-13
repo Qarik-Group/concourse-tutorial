e9c54a3680f533aef4db69b22ba377f91119b7cd
Dr Nic Williams <drnicwilliams@gmail.com>
2015-04-12 18:40:20 -0700

    60 - build-save pipeline uploads to s3

--- 60_multi_jobs_bosh_micro_build/run.sh
+++ 60_multi_jobs_bosh_micro_build/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+stub=$1; shift
+stage=$1; shift
+
 set -e
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
@@ -11,19 +14,26 @@ realpath() {
     [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
 }
 
-stub=$1; shift
-if [[ "${stub}X" == "X" ]]; then
-  echo "USAGE: run.sh path/to/stub.yml"
+usage() {
+  echo "USAGE: run.sh path/to/stub.yml build-cli|build-save"
   exit 1
+}
+
+if [[ "${stub}X" == "X" ]]; then
+  usage
 fi
 stub=$(realpath $stub)
 if [[ ! -f ${stub} ]]; then
-  echo "USAGE: run.sh path/to/stub.yml"
-  exit 1
+  usage
 fi
 
+if [[ "${stage}" != "build-cli" && "${stage}" != "build-save" ]]; then
+  usage
+fi
+
+
 pushd $DIR
-  spiff merge templates/pipeline-final.yml templates/pipeline-build-cli.yml ${stub} > pipeline.yml
+  spiff merge templates/pipeline-final.yml templates/pipeline-${stage}.yml ${stub} > pipeline.yml
   yes y | fly configure -c pipeline.yml
   curl $ATC_URL/jobs/job-build-bosh-init-cli/builds -X POST
   fly watch -j job-build-bosh-init-cli
