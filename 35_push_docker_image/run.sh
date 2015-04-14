88c1c1a464d9cc7e6257a91fc775bb9ff76f297f
Dr Nic Williams <drnicwilliams@gmail.com>
2015-04-14 15:35:04 -0700

    upgrade 35 to use v0.45 fly configure --vars-from

--- 35_push_docker_image/run.sh
+++ 35_push_docker_image/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+stub=$1; shift
+
 set -e
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
@@ -11,20 +13,18 @@ realpath() {
     [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
 }
 
-stub=$1; shift
 if [[ "${stub}X" == "X" ]]; then
-  echo "USAGE: run.sh path/to/stub.yml"
+  echo "USAGE: run.sh path/to/credentials.yml"
   exit 1
 fi
 stub=$(realpath $stub)
 if [[ ! -f ${stub} ]]; then
-  echo "USAGE: run.sh path/to/stub.yml"
+  echo "USAGE: run.sh path/to/credentials.yml"
   exit 1
 fi
 
 pushd $DIR
-  spiff merge templates/pipeline-final.yml templates/pipeline-publish-resource-image.yml ${stub} > pipeline.yml
-  yes y | fly configure -c pipeline.yml
+  yes y | fly configure -c pipeline.yml --vars-from ${stub}
   curl $ATC_URL/jobs/job-publish/builds -X POST
   fly watch -j job-publish
 popd
