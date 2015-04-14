d50c020d84ea8d896c2745688d37b2fbb97068b3
Dr Nic Williams <drnicwilliams@gmail.com>
2015-04-14 15:22:59 -0700

    upgrade 12 to use v0.45 fly configure --vars-from

--- 12_run_tests_before_deploy/run.sh
+++ 12_run_tests_before_deploy/run.sh
@@ -11,18 +11,17 @@ realpath() {
 
 stub=$1; shift
 if [[ "${stub}X" == "X" ]]; then
-  echo "USAGE: run.sh path/to/stub.yml"
+  echo "USAGE: run.sh path/to/credentials.yml"
   exit 1
 fi
 stub=$(realpath $stub)
 if [[ ! -f ${stub} ]]; then
-  echo "USAGE: run.sh path/to/stub.yml"
+  echo "USAGE: run.sh path/to/credentials.yml"
   exit 1
 fi
 
 pushd $DIR
-  spiff merge templates/pipeline-final.yml templates/pipeline-base.yml ${stub} > pipeline.yml
-  yes y | fly configure -c pipeline.yml
-  curl $ATC_URL/jobs/job-bump-version/builds -X POST
-  fly watch -j job-bump-version
+  yes y | fly configure -c pipeline.yml --vars-from $stub
+  curl $ATC_URL/jobs/job-test-deploy-app/builds -X POST
+  fly watch -j job-test-deploy-app
 popd
