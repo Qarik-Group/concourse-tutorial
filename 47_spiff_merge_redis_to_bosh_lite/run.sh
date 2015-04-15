e930ddd210baa434acc694895ee9bed2d89758a6
Dr Nic Williams <drnicwilliams@gmail.com>
2015-04-15 11:48:29 -0500

    47 - update job name; trying fix for spiff install

--- 47_spiff_merge_redis_to_bosh_lite/run.sh
+++ 47_spiff_merge_redis_to_bosh_lite/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 stub=$1; shift
+stage=$1; shift
 set -e
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
@@ -12,18 +13,31 @@ realpath() {
     [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
 }
 
-if [[ "${stub}X" == "X" ]]; then
-  echo "USAGE: run.sh path/to/credentials.yml"
+usage() {
+  echo "USAGE: run.sh credentials.yml [build-task-image|bosh-deploy]"
   exit 1
+}
+
+if [[ "${stub}X" == "X" ]]; then
+  usage
 fi
 stub=$(realpath $stub)
 if [[ ! -f ${stub} ]]; then
-  echo "USAGE: run.sh path/to/credentials.yml"
-  exit 1
+  usage
 fi
 
+if [[ "${stage}" != "build-task-image" && "${stage}" != "bosh-deploy" ]]; then
+  usage
+fi
+
+
 pushd $DIR
-  yes y | fly configure -c pipeline.yml --vars-from ${stub}
-  curl $ATC_URL/jobs/job-deploy/builds -X POST
-  fly watch -j job-deploy
+  yes y | fly configure -c pipeline-${stage}.yml --vars-from ${stub}
+  if [[ "${stage}" == "build-task-image" ]]; then
+    curl $ATC_URL/jobs/job-build-task-image/builds -X POST
+    fly watch -j job-build-task-image
+  else
+    curl $ATC_URL/jobs/job-deploy/builds -X POST
+    fly watch -j job-deploy
+  fi
 popd
