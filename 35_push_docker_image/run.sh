0776f9c1fd1accd4bd9eca5b00df62d6be7b685e
Long Nguyen <long.nguyen11288@gmail.com>
2015-05-10 23:26:50 -0400

    removed all other files atc

--- 35_push_docker_image/run.sh
+++ 35_push_docker_image/run.sh
@@ -5,10 +5,10 @@ stub=$1; shift
 set -e
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-export ATC_URL=${ATC_URL:-"http://192.168.100.4:8080"}
+
 export fly_target=${fly_target:-tutorial}
 echo "Concourse API target ${fly_target}"
-echo "Concourse API $ATC_URL"
+
 echo "Tutorial $(basename $DIR)"
 
 realpath() {
--- 40_github_release_input/run.sh
+++ 40_github_release_input/run.sh
@@ -3,10 +3,10 @@
 set -e
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-export ATC_URL=${ATC_URL:-"http://192.168.100.4:8080"}
+
 export fly_target=${fly_target:-tutorial}
 echo "Concourse API target ${fly_target}"
-echo "Concourse API $ATC_URL"
+
 echo "Tutorial $(basename $DIR)"
 
 realpath() {
