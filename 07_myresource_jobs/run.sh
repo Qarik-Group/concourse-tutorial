e9ac36f0f18c8ad38a604c04dbd93350dd069d6d
James Bayer <jbayer@gopivotal.com>
2015-06-13 22:04:20 -0700

    fixed 07 for 0.54.0

--- 07_myresource_jobs/run.sh
+++ 07_myresource_jobs/run.sh
@@ -3,6 +3,7 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 export ATC_URL=${ATC_URL:-"http://192.168.100.4:8080"}
 export fly_target=${fly_target:-tutorial}
+export pipelinename=${pipelinename:-07_myresource_jobs}
 echo "Concourse API target ${fly_target}"
 echo "Concourse API $ATC_URL"
 echo "Tutorial $(basename $DIR)"
@@ -14,7 +15,7 @@ if [[ "${pipeline}" != "simple" && "${pipeline}" != "renamed" ]]; then
 fi
 
 pushd $DIR
-  yes y | fly -t ${fly_target} configure -c pipeline-${pipeline}-resource.yml
-  curl $ATC_URL/pipelines/main/jobs/job-hello-world/builds -X POST
-  fly -t ${fly_target} watch -j job-hello-world
+  yes y | fly -t ${fly_target} configure -c pipeline-${pipeline}-resource.yml --paused=false ${pipelinename}
+  curl $ATC_URL/pipelines/${pipelinename}/jobs/job-hello-world/builds -X POST
+  fly -t ${fly_target} watch -p ${pipelinename} -j job-hello-world
 popd
