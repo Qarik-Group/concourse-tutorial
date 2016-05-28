91822ae39f71c9c8f413063a9e93b82ae6f200fb
Dr Nic Williams <drnicwilliams@gmail.com>
2016-05-28 09:14:28 -0700

    70 - add script to create a slack message from pivnet update

---
+++ 70_pivnet_updates/create-notification-message.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+version=$(cat pivnet-stemcells/version)
+release_type=$(cat pivnet-stemcells/metadata.json | jq -r ".release.release_type")
+description=$(cat pivnet-stemcells/metadata.json | jq -r ".release.description")
+
+echo $release_type
+echo $description
+
+cat > pivnet-message/message << EOF
+*${release_type}* ${version}
+${description}
+https://network.pivotal.io/products/${slug}
+EOF
