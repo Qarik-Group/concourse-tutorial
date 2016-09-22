ecd4dd713b36d658d5a2cbedd98d43a18910f1ee
Michael Brodhead <mkb@orthogonal.org>
2016-09-22 14:04:16 -0700

    Troubleshooting nokogiri install, starting on build.

--- test/build.sh
+++ test/build.sh
@@ -6,14 +6,14 @@ set -e
 ./01_*/run.sh
 ./02_*/run.sh
 ./03_*/run.sh
-./06_*/run.sh
-./07_*/run.sh simple
-./07_*/run.sh renamed
-./08_*/run.sh
-./09_*/run.sh ls-abc-xyz
-./09_*/run.sh ls-abc
-./09_*/run.sh pretty-ls
-./10_*/run.sh
+# ./06_*/run.sh
+# ./07_*/run.sh simple
+# ./07_*/run.sh renamed
+# ./08_*/run.sh
+# ./09_*/run.sh ls-abc-xyz
+# ./09_*/run.sh ls-abc
+# ./09_*/run.sh pretty-ls
+# ./10_*/run.sh
 
 # # ./12_*/run.sh credentials.yml
 
