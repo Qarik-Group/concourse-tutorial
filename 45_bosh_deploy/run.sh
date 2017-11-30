aa00214dddd19b2f68f082901cee7acfb75c1cf1
Ruben Koster <superruup@gmail.com>
2017-11-30 16:00:41 +0100

    Revert "configure syntax in line 33 unnecessary"

--- 45_bosh_deploy/run.sh
+++ 45_bosh_deploy/run.sh
@@ -30,7 +30,7 @@ if [[ ! -f ${stub} ]]; then
 fi
 
 pushd $DIR
-  fly sp -t ${fly_target} -c pipeline.yml -p main --load-vars-from ${stub} -n
+  fly sp -t ${fly_target} configure -c pipeline.yml -p main --load-vars-from ${stub} -n
   fly -t ${fly_target} unpause-pipeline --pipeline main
   fly -t ${fly_target} trigger-job -j main/job-deploy
   fly -t ${fly_target} watch -j main/job-deploy
