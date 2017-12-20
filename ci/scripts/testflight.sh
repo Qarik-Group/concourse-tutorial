0d5decfc30c2a49d188e334cedba2c3dbb2fa685
Dr Nic Williams <drnicwilliams@gmail.com>
2017-12-20 22:19:35 +1100

    store certs to files

--- ci/scripts/testflight.sh
+++ ci/scripts/testflight.sh
@@ -2,8 +2,6 @@
 
 set -eu
 
-cd ${REPO_ROOT:?required}
-
 if [[ "${FLY_CACERT:-X}" == "X" ]]; then
   fly --target tutorial login \
       --concourse-url ${FLY_URL:?required} \
@@ -11,21 +9,25 @@ if [[ "${FLY_CACERT:-X}" == "X" ]]; then
       --password      ${FLY_PASSWORD:?required} \
       --team-name main
 else
+  echo "$FLY_CACERT" > fly.cacert
   fly --target tutorial login \
       --concourse-url ${FLY_URL:?required} \
       --username      ${FLY_USERNAME:?required} \
       --password      ${FLY_PASSWORD:?required} \
       --team-name main \
-      --ca-cert       ${FLY_CACERT}
+      --ca-cert       fly.cacert
 fi
 
+echo "${CREDHUB_CACERT:?required}" > credhub.cacert
 credhub login \
       --server  ${CREDHUB_URL:?required} \
-      --ca-cert ${CREDHUB_CACERT:?required} \
+      --ca-cert credhub.cacert \
       --username ${CREDHUB_USERNAME:?required} \
       --password ${CREDHUB_PASSWORD:?required}
 
 export fly_target=tutorial
+
+cd ${REPO_ROOT:?required}
 
 ./tutorials/test-pipeline-vars.sh
 
