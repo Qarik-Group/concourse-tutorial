b2c32aed85165dbcfb2107eb45887341eeff43bf
Dr Nic Williams <drnicwilliams@gmail.com>
2017-12-21 11:06:23 +1000

    ci image has python3 + pip3 installed now

--- ci/scripts/mkdocs-build.sh
+++ ci/scripts/mkdocs-build.sh
@@ -2,9 +2,6 @@
 
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
-apt-get update
-apt-get install -y python3-pip
-pip3 install --upgrade pip
 pip3 install mkdocs-material
 
 git clone ${REPO_ROOT} ${SITE_ROOT}
