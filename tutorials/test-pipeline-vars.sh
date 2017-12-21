cf8f59fcf249cb4cb5f81bf77d8a34fe59e41d92
Dr Nic Williams <drnicwilliams@gmail.com>
2017-12-21 10:49:44 +1000

    deploy pipeline to 'concourse-tutorial' team

--- tutorials/test-pipeline-vars.sh
+++ tutorials/test-pipeline-vars.sh
@@ -3,17 +3,18 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
+team=${team:-concourse-tutorial}
 variables=$(cat */*/pipeline*.yml | grep "((" | awk '{print $2}' | sed -e "s%((%%" | sed -e "s%)).*%%" | sort | uniq)
 errors=
 for var in $variables; do
   if [[ "$var" == "cat-name" || "$var" == "dog-name" ]]; then
-    if credhub get -n /concourse/main/$var >/dev/null 2>/dev/null ; then
-      echo "Deleting variable /concourse/main/$var as it is expected to be missing"
-      credhub delete -n /concourse/main/$var
+    if credhub get -n /concourse/$team/$var >/dev/null 2>/dev/null ; then
+      echo "Deleting variable /concourse/$team/$var as it is expected to be missing"
+      credhub delete -n /concourse/$team/$var
     fi
   else
-    if ! credhub get -n /concourse/main/$var >/dev/null 2>/dev/null &&
-       ! credhub get -n /concourse/main/tutorial-pipeline/$var >/dev/null 2>/dev/null ; then
+    if ! credhub get -n /concourse/$team/$var >/dev/null 2>/dev/null &&
+       ! credhub get -n /concourse/$team/tutorial-pipeline/$var >/dev/null 2>/dev/null ; then
       echo "Not found $var"
       errors=1
     fi
