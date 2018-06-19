0e2f4a4fc0e68fa42d69828113a96b4df73514cb
Dr Nic Williams <drnicwilliams@gmail.com>
2018-06-19 10:07:50 +1000

    install pip3 if missing

--- ci/scripts/mkdocs-build.sh
+++ ci/scripts/mkdocs-build.sh
@@ -2,6 +2,13 @@
 
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
+
+if [[ "$(which pip3)X" == "X" ]]; then
+  apt-get update
+  apt-get install python3-pip -y
+fi
+
+
 pip3 install mkdocs-material
 
 git clone ${REPO_ROOT} ${SITE_ROOT}
