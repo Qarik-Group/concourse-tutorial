a3edcb321745ec4c5e2d80adf8bbe37bf7c8b025
Dr Nic Williams <drnicwilliams@gmail.com>
2018-09-23 14:32:31 -0400

    restrict mkdocs packages until can make time to upgrade https://ci2.starkandwayne.com/teams/starkandwayne/pipelines/concourse-tutorial/jobs/website-master/builds/32

--- ci/scripts/mkdocs-build.sh
+++ ci/scripts/mkdocs-build.sh
@@ -2,14 +2,9 @@
 
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
-
-if [[ "$(which pip3)X" == "X" ]]; then
-  apt-get update
-  apt-get install python3-pip -y
-fi
-
-
-pip3 install mkdocs-material
+apt-get update
+apt-get install -y python3-pip
+pip3 install 'pymdown-extensions<5.0' 'Markdown<3.0' 'mkdocs<1.0' 'mkdocs-material<3.0'
 
 git clone ${REPO_ROOT} ${SITE_ROOT}
 cd ${SITE_ROOT}
