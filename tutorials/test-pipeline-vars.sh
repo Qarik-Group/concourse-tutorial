d715ff0164746746f72f5fccb33b564ff86f3776
ramonskie <ramonmakkelie@gmail.com>
2020-01-21 10:31:01 +0100

    move to pipes and use vault as creds manager

--- tutorials/test-pipeline-vars.sh
+++ tutorials/test-pipeline-vars.sh
@@ -3,21 +3,32 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
+credman=${credmanager:-credhub}
+case $credman in
+  credhub)
+    cmdget="credhub get -n"
+    cmddel="credhub delete -n"
+  vault)
+    cmdget="safe get"
+    cmddel="safe delete"
+esac
+
 team=${team:-concourse-tutorial}
 variables=$(cat */*/pipeline*.yml | grep "((" | awk '{print $2}' | sed -e "s%((%%" | sed -e "s%)).*%%" | sort | uniq)
 errors=
 for var in $variables; do
   if [[ "$var" == "cat-name" || "$var" == "dog-name" ]]; then
-    if credhub get -n /concourse/$team/$var >/dev/null 2>/dev/null ; then
-      echo "Deleting variable /concourse/$team/$var as it is expected to be missing"
-      credhub delete -n /concourse/$team/$var
-    fi
+      if ${cmdget} /concourse/$team/$var >/dev/null 2>/dev/null ; then
+        echo "Deleting variable /concourse/$team/$var as it is expected to be missing"
+        ${cmddel} /concourse/$team/$var
+      fi
   else
-    if ! credhub get -n /concourse/$team/$var >/dev/null 2>/dev/null &&
-       ! credhub get -n /concourse/$team/tutorial-pipeline/$var >/dev/null 2>/dev/null ; then
+    if ! ${cmdget} /concourse/$team/$var >/dev/null 2>/dev/null &&
+       ! ${cmdget} /concourse/$team/tutorial-pipeline/$var >/dev/null 2>/dev/null ; then
       echo "Not found $var"
       errors=1
     fi
+
   fi
 done
 
