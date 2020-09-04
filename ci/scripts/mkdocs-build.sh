610ff9efcae27f6a3038deffbe9b773997ca3d45
Tyler Poland <tpoland@starkandwayne.com>
2020-09-04 16:58:41 -0400

    Branding and modernization
    Add S&W Logo and links along with credit for open source contributors.
    Modernize dependency versions

--- ci/scripts/mkdocs-build.sh
+++ ci/scripts/mkdocs-build.sh
@@ -4,7 +4,7 @@ export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 apt-get update
 apt-get install -y python3-pip
-pip3 install 'pymdown-extensions<5.0' 'Markdown<3.0' 'mkdocs<1.0' 'mkdocs-material<3.0'
+pip3 install 'pymdown-extensions<=8.0' 'Markdown<=3.2.2' 'mkdocs<=1.1.2' 'mkdocs-material<=5.5.9'
 
 git clone ${REPO_ROOT} ${SITE_ROOT}
 cd ${SITE_ROOT}
